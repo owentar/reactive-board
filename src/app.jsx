b513432f9f55e029a5ec09a42695e00079d9d250
Hernan Carrizo <owentar@gmail.com>
2018-03-10 20:02:46 +0000

    introduce actions to the app (add new card)

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,44 @@
 import xs from 'xstream';
 
-const model = () => {
-  return xs.of({
+const intent = ({ DOM }) => ({
+  add$: DOM.select('.add-card').events('click').map(e => e.target.value),
+});
+
+const model = ({ add$ }) => {
+  const newCard$ = add$.map(column => ({ column })).startWith(null);
+  const data$ = xs.of({
     columns: ['TODO', 'Doing', 'Done'],
+    cards: [],
   });
+
+  return xs.combine(data$, newCard$);
 };
 
-const renderColumns = columns => {
+const renderColumns = (columns, newCard) => {
   return columns.map(column => (
     <div className="col">
       <h1>{ column }</h1>
       <div className="container cards-column">
-        <button type="button" className="btn btn-secondary">Add card</button>
+        <button type="button" className="add-card btn btn-secondary" value={column}>Add card</button>
       </div>
     </div>
   ));
 };
 
 const view = (state$) => {
-  return state$.map(({ columns }) => (
+  return state$.map(([{ columns }, newCard]) => (
     <div className="container-fluid">
       <div className="row">
-        { renderColumns(columns) }
+        { renderColumns(columns, newCard) }
       </div>
     </div>
   ));
 };
 
 export function App (sources) {
-  const state$ = model();
+  const actions = intent(sources);
+
+  const state$ = model(actions);
 
   const vtree$ = view(state$);
 
